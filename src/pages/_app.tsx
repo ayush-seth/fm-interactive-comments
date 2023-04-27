220f8af9adc90a6780d73a372b5ce509239f11b3
Ayush Seth <ayush-seth@outlook.com>
2023-04-27 22:40:01 +0530

    minor cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Interactive Comments Section</title>
+        <meta
+          name="description"
+          content="A frontend mentor project - Interactive Comments Section"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
